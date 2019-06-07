b81db4561e8a3c563ae67708b9975da03732786c
George Hahn <george.hahn@smartthings.com>
2019-06-06 22:30:52 -0500

    Log everything

--- wyze/src/main.rs
+++ wyze/src/main.rs
@@ -1,4 +1,8 @@
 extern crate libusb;
+extern crate log;
+extern crate simple_logger;
+
+use log::{trace};
 
 const HUB_VENDOR_ID: u16 = 0x1A86;
 const HUB_PRODUCT_ID: u16 = 0xE024;
@@ -38,21 +42,29 @@ pub fn get_hubs<'a>(context: &'a libusb::Context) -> Vec<WyzeHub<'a>> {
 }
 
 fn main() {
+    simple_logger::init().unwrap();
+
     let context = libusb::Context::new().unwrap();
     let mut hubs = get_hubs(&context);
-    println!("Found {} hubs", hubs.len());
+    println!("Found {} bridge(s)", hubs.len());
     if hubs.len() == 0 {
         return;
     }
 
+    trace!("Open bridge");
     let hub = &mut hubs[0].handle.open().unwrap();
 
+    trace!("Reset bridge");
     hub.reset().unwrap();
+
+    trace!("Set active config");
     hub.set_active_configuration(0x00).unwrap();
+
+    trace!("Claim interface");
     hub.claim_interface(0x0000).unwrap();
 
     let msg1 = vec![0xAA, 0x55, 0x43, 0x03, 0x27, 0x01, 0x6C];
-    println!("Trying to write");
+    trace!("Write a message");
     hub.write_control(
         0x21,
         0x09,
@@ -65,12 +77,10 @@ fn main() {
 
     let mut raw_data = [0; 100];
 
+    trace!("Read a message");
     let len = hub
         .read_bulk(0x82, &mut raw_data, std::time::Duration::new(1, 0))
         .unwrap();
 
-    println!("Read {:?}, data: {:?}", len, &raw_data[..len]);
-    for i in 0..len {
-        print!("0x{:02X} ", raw_data[i]);
-    }
+    trace!("Read {:?}, data: {:X?}", len, &raw_data[..len]);
 }
